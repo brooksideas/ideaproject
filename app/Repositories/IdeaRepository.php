174fb303c5d47424f8d4be2a1c2198586cb1d272
Brook Tarekegn <brooksideas@gmail.com>
2022-07-04 01:34:15 +0300

    Rewrite of all function with Pluck and WhereIn functions

--- app/Repositories/IdeaRepository.php
+++ app/Repositories/IdeaRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use \App\Models\Idea;
+use \App\Models\User;
 
 
 class IdeaRepository
@@ -11,10 +12,14 @@ class IdeaRepository
     public function all()
     {
         // https://stackoverflow.com/questions/19852927/get-specific-columns-using-with-function-in-laravel-eloquent
-        $ideas = Idea::orderBy('goal')
-            ->where('idea_owner', 1)
-            ->with('user')
-            ->join('users', 'users.id' , '=' , 'ideas.idea_owner')
+        // $ideas = Idea::orderBy('goal')
+        //     ->where('idea_owner', 1)
+        //     ->with('user')
+        //     ->join('users', 'users.id' , '=' , 'ideas.idea_owner')
+
+        // https://medium.com/backenders-club/using-pluck-and-wherein-in-laravel-2dce9fb5033b
+        $ideaOwnerIds = User::all()->pluck('id');
+        $ideas = Idea::whereIn("idea_owner", $ideaOwnerIds)
             ->get()
             ->map(function ($idea) {
                 return [
